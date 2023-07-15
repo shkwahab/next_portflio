e05ce168688b52abfd4656b2f1188c12357ff644
shkwahab <wahab209266@gmail.com>
2023-07-15 16:21:09 +0500

    porfolio

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,7 +16,7 @@ const NotFound=()=> {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                         </svg>
                     </p>
-                    <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Coming Soon</h1>
+                    <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page Not Found</h1>
                     <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn't exist. Here are some helpful links:</p>
 
                     <div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
